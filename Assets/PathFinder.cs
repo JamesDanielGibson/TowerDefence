9232a3ec662ed862e8ce967bff6f8aca198ef76c
JamesDanielGibson <g18g0723@campus.ru.ac.za>
2020-07-04 22:41:04 +0200

    addedto the path finding class.

--- Assets/PathFinder.cs
+++ Assets/PathFinder.cs
@@ -7,13 +7,69 @@ public class PathFinder : MonoBehaviour
 {
     [SerializeField] WayPoint startPoint;
     [SerializeField] WayPoint endPoint;
+
     Dictionary<Vector2Int, WayPoint> grid = new Dictionary<Vector2Int, WayPoint>();
+    Queue<WayPoint> queue = new Queue<WayPoint>();
+    bool isRunning = true;
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
     // Start is called before the first frame update
     void Start()
     {
         
         LoadBlocks();
         ColorStartAndEnd();
+        //ExploreNeighbours();
+        PathFind();
+    }
+
+    private void PathFind()
+    {
+        queue.Enqueue(startPoint);
+        while(queue.Count>0&& isRunning)
+        {
+            var searchItem = queue.Dequeue();
+            HaltStartEqualsEnd();
+            ExploreNeighbours(searchItem);
+            //if (!isRunning) { break; }
+        }
+    }
+
+    void HaltStartEqualsEnd()
+    {
+        if (startPoint.GetGridPos() == endPoint.GetGridPos())
+        {
+            print("but im already here?");// todo remove this later
+            isRunning = false;
+        }
+    }
+    void ExploreNeighbours(WayPoint from)
+    {
+        if (!isRunning) { return; }
+        foreach(Vector2Int direction in directions)
+        {
+            Vector2Int neighbourCoords = from.GetGridPos() + direction;
+            print("exploring"+ neighbourCoords);//todo remove this later
+            try
+            {
+                WayPoint neighbour = grid[neighbourCoords];
+                neighbour.SetTopColor(Color.blue);// move to somewhere else
+                queue.Enqueue(neighbour);
+                print("queueing neighbour");
+            }
+            catch
+            {
+                print("Skipping");// remove this later
+            }
+            
+        }
     }
 
     private void ColorStartAndEnd()
